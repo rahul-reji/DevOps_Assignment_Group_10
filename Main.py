dbee12d0e04ea67afde407e5b6d5bd6824e4086e
rahul-reji <79362576+rahul-reji@users.noreply.github.com>
2021-10-06 17:49:10 +0530

    Pushing the main.py file.
    Collaborators should create their functions and update the Main.py.

---
+++ Main.py
@@ -0,0 +1,32 @@
+#include files to be added here
+
+while(True):
+    print("Hello User! What would you like to do today? -\n" \
+            "1. Calculator\n" \
+            "2. Calculate Factorial\n" \
+            "3. Check if Prime or Not\n" \
+            "4. Check if Palindrome or Not\n" \
+            "5. Exit\n")
+    
+    
+    # Take input from the user 
+    select = int(input("Select operations form 1, 2, 3, 4 : "))
+    
+    if select == 1:
+        print(1)
+        # Rahul function
+    
+    elif select == 2:
+        print(2)
+        # Anju function
+    
+    elif select == 3:
+        print(3)
+        # Shwetha function
+        
+    elif select == 4:
+        print(4)
+        # Pawan function 
+
+    elif select == 5:
+        quit()
